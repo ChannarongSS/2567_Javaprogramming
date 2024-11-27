409c9414921eb182b8e8414f3470de13de5cce6f
ChannarngSaeung <2413110285@TNISTUDENT.AC.TH>
2024-11-27 11:14:26 +0700

    javaExample

---
+++ LabsSheet01/src/javaLab101.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class javaLab101 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner Input = new Scanner(System.in);
+		
+		
+
+	}
+
+}
