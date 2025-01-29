83cab4703116d2a8ad0aa6d14b6efa86a505bea1
ChannarngSaeung <2413110285@TNISTUDENT.AC.TH>
2025-01-29 11:31:16 +0700

    Labsheet08

---
+++ Labsheet08/src/Product.java
@@ -0,0 +1,45 @@
+
+public class Product {
+	private String productId;
+	private int  productunit;
+	private double  productprice;
+	
+	public void setId (String pid) {
+		productId = pid ;
+		
+		
+	}
+	
+	public String getid() {
+		return this.productId;
+		
+	}
+	
+	public void setUnit(int unit) {
+		productunit = unit;
+		
+	}
+	
+	public int getUnit() {
+		return this.productunit;
+		
+	}
+	
+	public void setprice(double price) {
+		productprice = price;
+	}
+	
+	public double getprice() {
+		return this.productprice;
+		
+	}
+	
+	public double calculate() {
+		double Tax = productunit *  productprice;
+		return Tax;
+		
+	}
+	
+}
+
+
