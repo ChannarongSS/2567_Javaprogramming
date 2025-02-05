e7c22c4631807880cb0eb460d04aeeee2d1d1302
ChannarngSaeung <2413110285@TNISTUDENT.AC.TH>
2025-02-05 11:26:15 +0700

    Labsheet09

---
+++ Labsheet09/src/Director.java
@@ -0,0 +1,38 @@
+
+public class Director {
+	private String name; 
+	private String email;
+	private char gender;
+	
+	Director(String name , String email, char gender) {
+		this.name = name;
+		this.email = email;
+		this.gender = gender;
+	}	
+	
+	Director(String name , String email) {
+		this.name = name;
+		this.email = email;
+	} 
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public String getGenderName() {
+		return  this.name;
+	}
+	
+	public String toString() {
+		return getName() + "(" + getEmail() + ")("+ getGenderName()+ "%)";
+	}
+	
+	
+	
+	
+	
+}
---
+++ Labsheet09/src/Movie.java
@@ -0,0 +1,23 @@
+
+public class Movie {
+	private String id;
+	private String name;
+	private Director Director;
+	
+	Movie(String id,String  name,Director director){
+		this.id = id;
+		this.name = name;
+		this.Director = director;
+	}
+	
+	public void Movie() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
