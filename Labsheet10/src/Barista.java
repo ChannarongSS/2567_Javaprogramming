eadf2686606bfa947ae76354d9d175bd904bcad8
ChannarngSaeung <2413110285@tnistudent.ac.th>
2025-02-19 11:16:55 +0700

    PracticeLabsheet10

---
+++ Labsheet10/src/Barista.java
@@ -0,0 +1,23 @@
+
+public class Barista {
+	private String name;
+	private char gender;
+	
+	 Barista(String name, char gender) {
+		this.name = name;
+		this.gender = gender;
+	}
+	
+	Barista() {
+		this(null,' ');	
+		}
+	
+	public String  getName() {
+		return this.name;
+	}
+	
+	public String getgenderName() {
+		
+	}
+	
+}
